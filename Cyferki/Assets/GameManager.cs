f8ab14845105614479d41fb9113c7a668b14aa13
Sztiffen <domianpiotr111@gmail.com>
2021-01-12 22:06:45 +0100

    tworzenie nowych cyferek by coroutine

---
+++ Cyferki/Assets/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] GameObject btnCyferkaPrefab;
+    [SerializeField] Transform kontenernaCyferki;
+    void Start()
+    {
+        StartCoroutine(TworzNowaCyferke());
+    }
+
+    IEnumerator TworzNowaCyferke()
+    {
+        GameObject nowaCyferka = Instantiate(btnCyferkaPrefab, kontenernaCyferki);
+        int losowaPozycjaX = Random.Range(-140,141);
+        nowaCyferka.GetComponent<RectTransform>().anchoredPosition = new Vector2(losowaPozycjaX, 400f);
+        yield return new WaitForSeconds(1f);
+        StartCoroutine(TworzNowaCyferke());
+    }
+    void Update()
+    {
+        
+    }
+}
