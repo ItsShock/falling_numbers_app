c3d80babc6b702b1366f1763f98647ef6e6b548a
Sztiffen <domianpiotr111@gmail.com>
2021-01-12 20:34:54 +0100

    funkcja opadania

---
+++ Cyferki/Assets/Cyferka.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cyferka : MonoBehaviour
+{
+    RectTransform rt;
+    GameObject txtCyferka;
+    float predkoscOpadania = 100f;
+    void Start()
+    {
+        txtCyferka = gameObject.transform.GetChild(0).gameObject;
+        PrzypiszWartoscTxt();
+        rt = GetComponent<RectTransform>();
+    }
+
+    void PrzypiszWartoscTxt()
+    {
+        int LosowaLiczba = Random.Range(1, 7);
+        txtCyferka.GetComponent<Text>().text = LosowaLiczba.ToString();
+    }
+
+    public void PobierzWartosczPolaTxt()
+    {
+        int wybranaLiczba = System.Int32.Parse(txtCyferka.GetComponent<Text>().text);
+        print(wybranaLiczba);
+    }
+
+    void SpadajwDol()
+    {
+        rt.anchoredPosition -= new Vector2(0f, 1f) * predkoscOpadania * Time.deltaTime;
+    }
+
+    void UsunCyferke()
+    {
+        Destroy(gameObject);
+    }
+    
+    void Update()
+    {
+        SpadajwDol();
+        if(rt.anchoredPosition.y < -400)
+        {
+            UsunCyferke();
+        }
+    }
+}
